758ee8edcc3f7727df7c129d674cba01989fc1ee
Julein "djakk <julien.tadashi@gmail.com>
2019-04-02 18:28:14 +0200

    Added some logs

--- install_mod_tile.sh
+++ install_mod_tile.sh
@@ -1,13 +1,17 @@
 cd
 mkdir src
 
+echo "telecharger mod_tile"
 cd ~/src
 git clone git://github.com/SomeoneElseOSM/mod_tile.git
 cd mod_tile
+echo "fin de telecharger mod_tile"
 
+echo "installer mod_tile"
 ./autogen.sh
 ./configure
 make
 make install
 make install-mod_tile
 ldconfig
+echo "fin de installer mod_tile"
