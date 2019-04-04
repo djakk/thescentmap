42a54903abac45102581a0665dc29530ea1c8f4c
Julein "djakk <julien.tadashi@gmail.com>
2019-04-04 18:27:30 +0200

    Recursive function "calculate_the_geometries"

--- newick_io/to_database.py
+++ newick_io/to_database.py
@@ -18,21 +18,18 @@ def save_to_postgresql(the_tree, the_url_to_the_database):
   print("inside 'save_to_postgresql'")
   print(the_tree.name)
   print(the_tree.ascii_art(show_internal=False, strict=True))
-    
+  
+  # put the descendants on an half-circle and the father in the middle of the halfed circle
+  calculate_the_geometries(the_tree)
+  for a_node in the_tree.walk():
+    print(a_node.geometry)
   
   the_connection = psycopg2.connect(the_url_to_the_database)
   
   the_cursor = the_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
   the_cursor.execute("""DELETE FROM myTable;""")
   the_cursor.close()
-  
-  # put the descendants on an half-circle and the father in the middle of the halfed circle
-  try:
-    the_geometry_of_the_center = the_tree.geometry
-  except AttributeError:
-    the_geometry_of_the_center = shapely.geometry.Point(0, 0)
-  the_tree.geometry = the_geometry_of_the_center
-  
+    
   the_counter = 1
   for a_sub_tree in the_tree.descendants:
     
@@ -55,10 +52,29 @@ VALUES (%(geometry)s, %(properties)s);\
     the_cursor.close()
     
     the_counter += 1
-    
-    # recursive call
-    save_to_postgresql(a_sub_tree, the_url_to_the_database)
   
   the_connection.commit()
   the_connection.close()
   
+  
+  def calculate_the_geometries(the_tree):
+    """recursive function"""
+    try:
+      the_geometry_of_the_center = the_tree.geometry
+    except AttributeError:
+      the_geometry_of_the_center = shapely.geometry.Point(0, 0)
+      the_tree.geometry = the_geometry_of_the_center
+    
+    the_counter = 1
+    
+    for a_sub_tree in the_tree.descendants:
+
+      the_sub_tree_longitude = the_geometry_of_the_center.coords[0][0] + 100 *the_counter
+      the_sub_tree_latitude = the_geometry_of_the_center.coords[0][1] + 100
+      the_sub_tree_last_point = shapely.geometry.Point(the_sub_tree_longitude, the_sub_tree_latitude)
+      #the_line_as_a_shapely_geometry = shapely.geometry.LineString([the_geometry_of_the_center, the_sub_tree_last_point])
+      a_sub_tree.geometry = the_sub_tree_last_point
+      calculate_the_geometries(a_sub_tree)
+      
+      the_counter += 1
+
