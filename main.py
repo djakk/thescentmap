f4ea4e21cfe4d063f376ca9030a6b432ca3b87c2
Julein "djakk <julien.tadashi@gmail.com>
2019-04-04 13:01:13 +0200

    Added newick_io.to_database

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import os
 import pika
 import newick
 
+import newick_io
+import newick_io.to_database
+
 
 print("Coucou ! (from print)")
 sys.stdout.write("Coucou ! (from sys.stdout.write)")
@@ -16,8 +19,6 @@ sys.stdout.flush()
 url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')
 params = pika.URLParameters(url_str)
 
-with io.open('the_example_newick_tree.tre', encoding='utf8') as fp:
-  trees = newick.load(fp)
 
 
 def aPrintingFunction(ch, method, properties, body):
@@ -29,7 +30,12 @@ def aPrintingFunction(ch, method, properties, body):
 def theCallbackFunction(ch, method, properties, body):
     print(u"inside theCallbackFunction")
     
-        
+    with io.open('the_example_newick_tree.tre', encoding='utf8') as fp:
+      trees = newick.load(fp)
+      
+      # -> postgresql database
+      newick_io.to_database.save_to_postgresql(trees[0], os.environ.get('DATABASE_URL'))
+    
     # datas are ready inside the postgresql database : send a response back to NodeJS
     the_connection = pika.BlockingConnection(params)
     the_channel = the_connection.channel()
