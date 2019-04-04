3c3756897e83d0fe37c4fb48d0fcc82e9d2dbfa8
Julein "djakk <julien.tadashi@gmail.com>
2019-04-04 08:05:37 +0200

    Reading the_example_newick_tree.tre

--- main.py
+++ main.py
@@ -3,5 +3,13 @@
 import newick
 import mapnik
 
+import io
+
+
 print("Hello !")
 the_map = mapnik.Map(256,256)
+
+with io.open('the_example_newick_tree.tre', encoding='utf8') as fp:
+  trees = newick.load(fp)
+
+print("tree loaded")
