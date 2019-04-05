40e8b0a45970a3f0b0dfcfb876ddc7dcf2ddc1c1
Julein "djakk <julien.tadashi@gmail.com>
2019-04-05 16:58:18 +0200

    Handling index.html at server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ mapnik.register_default_input_plugins();
 var port = process.env.PORT || 3000;
 //var stylesheet = './stylesheet.xml';
 
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
+
 http.createServer(function(req, res) {
   
   // trying to call python function aPrintingFunction through CloudAMPQ (queueing add-on)
@@ -58,68 +66,81 @@ http.createServer(function(req, res) {
     return ok;
   }).then(null, console.warn);
   
+  var query = url.parse(req.url.toLowerCase(), true).query;
   
   res.writeHead(500, {'Content-Type': 'text/plain'});
   
-  
-  // map with just a style
-  // eventually the api will support adding styles in javascript (!)
-  var s = '<Map srs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs">';
-  s += '<Style name="points">';
-  s += ' <Rule>';
-  s += '  <PointSymbolizer />';
-  s += ' </Rule>';
-  s += '</Style>';
-  s += '<Style name="lines">';
-  s += ' <Rule>';
-  s += '  <LineSymbolizer stroke="[colour]" />';
-  s += ' </Rule>';
-  s += ' <Rule>';
-  s += '  <TextSymbolizer placement="line" face-name="DejaVu Sans Book">[the_text]</TextSymbolizer>';
-  s += ' </Rule>';
-  s += '</Style>';
-  s += '</Map>';
-  
-  // create map object
-  var map = new mapnik.Map(1024, 1024);
-  map.fromStringSync(s);
-  
-  console.log("creating the map …");
-  
-  the_database_url = new URL(process.env.DATABASE_URL);
-  var options = {
-    type: 'postgis',
-    host: the_database_url.hostname, 
-    port: the_database_url.port, 
-    dbname: the_database_url.pathname.substring(1), 
-    user: the_database_url.username,
-    password: the_database_url.password, 
-    table: "(SELECT geometry, 'red' AS colour, properties->'name' AS the_text FROM mytable) AS roads", 
-    geometry_field: 'geometry'
-  };
-  
-  var the_points_datasource = new mapnik.Datasource(options);
-  var the_points_layer = new mapnik.Layer('points\' layer', "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
-  the_points_layer.datasource = the_points_datasource;
-  //the_points_layer.styles = ['lines', 'points'];
-  the_points_layer.styles = ['lines'];
-  map.add_layer(the_points_layer);
-  
-  console.log("creating the image …");
-  map.zoomAll();
-  var the_image__for_the_map = new mapnik.Image(1024, 1024);
-  map.render(the_image__for_the_map, function(err,im) {
-    if (err) {
-      res.end(err.message);
-    } else {
-      im.encode('png', function(err,buffer) {
-        if (err) {
-          res.end(err.message);
-        } else {
-          res.writeHead(200, {'Content-Type': 'image/png'});
-          res.end(buffer);
-        }
+  if (!query || isEmpty(query)) {
+    try {
+      res.writeHead(200, {
+      'Content-Type': 'text/html'
       });
+      if (req.url == '/') {
+          res.end(fs.readFileSync('./index.html'));
+      }
+    } catch (err) {
+          res.end('Not found: ' + req.url);      
     }
-  });
+  } else {
+    // map with just a style
+    // eventually the api will support adding styles in javascript (!)
+    var s = '<Map srs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs">';
+    s += '<Style name="points">';
+    s += ' <Rule>';
+    s += '  <PointSymbolizer />';
+    s += ' </Rule>';
+    s += '</Style>';
+    s += '<Style name="lines">';
+    s += ' <Rule>';
+    s += '  <LineSymbolizer stroke="[colour]" />';
+    s += ' </Rule>';
+    s += ' <Rule>';
+    s += '  <TextSymbolizer placement="line" face-name="DejaVu Sans Book">[the_text]</TextSymbolizer>';
+    s += ' </Rule>';
+    s += '</Style>';
+    s += '</Map>';
+  
+    // create map object
+    var map = new mapnik.Map(1024, 1024);
+    map.fromStringSync(s);
+
+    console.log("creating the map …");
+
+    the_database_url = new URL(process.env.DATABASE_URL);
+    var options = {
+      type: 'postgis',
+      host: the_database_url.hostname, 
+      port: the_database_url.port, 
+      dbname: the_database_url.pathname.substring(1), 
+      user: the_database_url.username,
+      password: the_database_url.password, 
+      table: "(SELECT geometry, 'red' AS colour, properties->'name' AS the_text FROM mytable) AS roads", 
+      geometry_field: 'geometry'
+    };
+
+    var the_points_datasource = new mapnik.Datasource(options);
+    var the_points_layer = new mapnik.Layer('points\' layer', "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
+    the_points_layer.datasource = the_points_datasource;
+    //the_points_layer.styles = ['lines', 'points'];
+    the_points_layer.styles = ['lines'];
+    map.add_layer(the_points_layer);
+
+    console.log("creating the image …");
+    map.zoomAll();
+    var the_image__for_the_map = new mapnik.Image(1024, 1024);
+    map.render(the_image__for_the_map, function(err,im) {
+      if (err) {
+        res.end(err.message);
+      } else {
+        im.encode('png', function(err,buffer) {
+          if (err) {
+            res.end(err.message);
+          } else {
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(buffer);
+          }
+        });
+      }
+    });
+  }
 }).listen(port);
