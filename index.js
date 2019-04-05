b2ceb840e550e32f6784cc5a3c323afe3b10c8cb
Julein "djakk <julien.tadashi@gmail.com>
2019-04-05 19:37:58 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function load_map()
-{
-    var mymap = L.map('mapid').setView([0.0, 0.0], 13);
-    
-    L.tileLayer('https://thescentmap.herokuapp.com/map/{id}/{z}/{x}/{y}.png', {
-      maxZoom: 18
-    }).addTo(mymap);
-}
