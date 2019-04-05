0ef90d7f58b1343557c344b04eb9743771bb50c7
Julein "djakk <julien.tadashi@gmail.com>
2019-04-05 18:10:57 +0200

    map will be around coordinates (0,0)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function load_map()
 {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([0.0, 0.0], 13);
     
     L.tileLayer('map/{z}/{x}/{y}.png', {
       maxZoom: 18
