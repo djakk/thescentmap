3700f5a2d1c84433f042845530539507f2780add
Julein "djakk <julien.tadashi@gmail.com>
2019-04-05 18:33:55 +0200

    Will it work with the full web address ?

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function load_map()
 {
     var mymap = L.map('mapid').setView([0.0, 0.0], 13);
     
-    L.tileLayer('/map/{id}/{z}/{x}/{y}.png', {
+    L.tileLayer('https://thescentmap.herokuapp.com/map/{id}/{z}/{x}/{y}.png', {
       maxZoom: 18
     }).addTo(mymap);
 }
