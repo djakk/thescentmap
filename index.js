82a712c46d5654c094f6bc737ac6fad6dd2983b0
Julein "djakk <julien.tadashi@gmail.com>
2019-04-05 18:08:33 +0200

    new js function load_map()

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('map/{z}/{x}/{y}.png', {
-    maxZoom: 18
-}).addTo(mymap);
+function load_map()
+{
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    
+    L.tileLayer('map/{z}/{x}/{y}.png', {
+      maxZoom: 18
+    }).addTo(mymap);
+}
