1725c1d159b4699b3db1640a90e996072f959470
Julein "djakk <julien.tadashi@gmail.com>
2019-04-05 16:00:07 +0200

    Fixing the calculation of the geometries

--- newick_io/to_database.py
+++ newick_io/to_database.py
@@ -67,9 +67,9 @@ def calculate_the_geometries(the_tree, the_counter):
   
   for a_sub_tree in the_tree.descendants:
     
-    the_sub_tree_longitude = the_geometry_of_the_center.coords[0][0] + (10 / the_counter) *math.cos(the_other_counter * 3.14 / the_number_of_descendants)
-    the_sub_tree_latitude = the_geometry_of_the_center.coords[0][1] + (10 / the_counter) *math.sin(the_other_counter * 3.14 / the_number_of_descendants)
-    the_sub_tree_last_point = shapely.geometry.Point(the_sub_tree_longitude, -1* the_sub_tree_latitude)
+    the_sub_tree_longitude = the_geometry_of_the_center.coords[0][0] - (10 / the_counter) *math.cos(the_other_counter * 3.14 / the_number_of_descendants)
+    the_sub_tree_latitude = the_geometry_of_the_center.coords[0][1] - (10 / the_counter) *math.sin(the_other_counter * 3.14 / the_number_of_descendants)
+    the_sub_tree_last_point = shapely.geometry.Point(the_sub_tree_longitude, the_sub_tree_latitude)
     #the_line_as_a_shapely_geometry = shapely.geometry.LineString([the_geometry_of_the_center, the_sub_tree_last_point])
     a_sub_tree.geometry = the_sub_tree_last_point
     calculate_the_geometries(a_sub_tree, the_counter +1)
